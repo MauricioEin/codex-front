7bcb6521759b1d57c0b33f616bbcdaeeb46da362
MauricioEin <mauricio.ein@gmail.com>
2023-02-05 12:17:50 +0200

    code textarea

--- src/views/code-block.vue
+++ src/views/code-block.vue
@@ -1,9 +1,8 @@
 <template>
   <section class="code-block">
     <h1>Code Block Page</h1>
+    <textarea></textarea>
 
-
-    
     <nav>
       <router-link to="/code/1">Previous</router-link>
       <router-link to="/code/2">Next</router-link>
@@ -14,5 +13,8 @@
 <script>
 export default {
   name: 'code-block',
+  async created() {
+    console.log(`this.$route.params.id:`, this.$route.params.id)
+  },
 }
 </script>
