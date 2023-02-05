57db8b957dfd958ffd56711d7514e3bd379b7432
MauricioEin <mauricio.ein@gmail.com>
2023-02-05 11:23:39 +0200

    block page paging links

--- src/views/code-block.vue
+++ src/views/code-block.vue
@@ -1,6 +1,13 @@
 <template>
-  <section>
+  <section class="code-block">
     <h1>Code Block Page</h1>
+
+
+    
+    <nav>
+      <router-link to="/code/1">Previous</router-link>
+      <router-link to="/code/2">Next</router-link>
+    </nav>
   </section>
 </template>
 
