b0377b206f73a785381fdbc3c31487bced0ba755
MauricioEin <mauricio.ein@gmail.com>
2023-02-07 15:27:20 +0200

    about layout

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="about flex column justify-center align-center">
+  <section class="about flex column justify-center align-center">
     <h2>Welcome to Codex</h2>
-    <h3>This is a platform for remote tutoring in JS, implementing:</h3>
-
-    <h4 class="list-header"> Front End</h4>
-    <ul>
-      <li>Vue.js3</li>
-      <li>Scss</li>
-      <li>VueX state Management</li>
-      <li>Highlight.js</li>
-      <li>Axios</li>
-    </ul>
-    <h4 class="list-header"> Back End</h4>
-    <ul>
-      <li>node.js</li>
-      <li>mongoDB</li>
-      <li>socket.io</li>
-    </ul>
-  </div>
+    <div class="stack-details">
+      <h3>This is a platform for remote tutoring in JS, implementing:</h3>
+      <div class="list-container flex">
+        <ul>
+          <li><h4 class="list-header"> Front End</h4></li>
+          <li>Vue.js3</li>
+          <li>Scss</li>
+          <li>VueX state Management</li>
+          <li>Highlight.js</li>
+          <li>Axios</li>
+        </ul>
+        <ul>
+          <li><h4 class="list-header"> Back End</h4></li>
+          <li>node.js</li>
+          <li>mongoDB</li>
+          <li>socket.io</li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
