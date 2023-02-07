93807422108122c187de9effb5ffe7e6c8926ed7
MauricioEin <mauricio.ein@gmail.com>
2023-02-07 23:12:36 +0200

    about page links

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,24 +1,39 @@
 <template>
   <section class="about flex column justify-center align-center">
-    <h2>Welcome to Codex</h2>
-    <div class="stack-details">
-      <h3>This is a platform for remote tutoring in JS, implementing:</h3>
-      <div class="list-container flex">
-        <ul>
-          <li><h4 class="list-header"> Front End</h4></li>
-          <li>Vue.js3</li>
-          <li>Scss</li>
-          <li>VueX state Management</li>
-          <li>Highlight.js</li>
-          <li>Axios</li>
-        </ul>
-        <ul>
-          <li><h4 class="list-header"> Back End</h4></li>
-          <li>node.js</li>
-          <li>mongoDB</li>
-          <li>socket.io</li>
-        </ul>
-      </div>
+    <h3>CodeX is a platform for remote tutoring in JS,<br>made by Mauricio Einhorn as a home interview task,<br>implementing:
+    </h3>
+    <div class="stack-details flex">
+      <a v-for="(repo, idx) in repos" :key="idx" :href="repo.link" target="_blank" class="repo-link">
+        <h4 class="list-header">{{ repo.title }}</h4>
+        <p v-for="(element, idx) in repo.elements" :key="idx">{{ element }}</p>
+      </a>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+      repos: [
+        {
+          link: 'https://github.com/MauricioEin/codex-front',
+          title: 'Front End',
+          elements: ['Vue.js3', 'Scss', 'Vuex State Management', 'Highlight.js', 'Axios']
+        },
+        {
+          link: 'https://github.com/MauricioEin/codex-back',
+          title: 'Back End',
+          elements: ['node.js','mongoDB','socket.io']
+        }
+      ]
+    }
+  },
+  methods: {
+    navigate() {
+      window.open('')
+    },
+  }
+}
+</script>
